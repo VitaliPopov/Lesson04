e9ecce20f31a80cffd26b246886b5a2123f701d7
Vitalii Popov <vetlo@yandex.ru>
2023-06-08 13:25:27 +0300

    If Else

---
+++ Lesson04_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dinara")
+{
+    Console.WriteLine("I love you, my Sweetheart!");
+}
+else
+{
+    Console.Write("Glad to see you! ");
+    Console.Write(username);
+}
